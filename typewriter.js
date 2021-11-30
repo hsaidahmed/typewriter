05f645f116dba49b73d70449fe48c8de01869fae
hsaidahmed <hashim.saidahmed@gmail.com>
2021-11-30 22:13:35 +0000

    commiting typewriter

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+let increment = 50;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += increment;
+}
+setTimeout(() => console.log("\n"),1550);
+
